d7fc0c3a03498c18aaed56ca81a480832cbdd262
bhuwanoj_ <bhuwanojha10@gmail.com>
2025-05-24 23:28:02 +0545

    news error handling added

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -21,11 +21,103 @@ const createNews = async (req, res) => {
 };
 
 const getNews = async (req, res) => {
-  const db = req.app.local.db;
+  const db = req.app.locals.db;
 
   try {
     const [news] = await db.execute("SELECT * FROM news");
-  } catch (error) {}
+    res.json({
+      status: "success",
+      data: news,
+    });
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Failed to get news",
+    });
+  }
 };
 
-module.exports = { createNews };
+const getNewsById = async (req, res) => {
+  const db = req.app.locals.db;
+  const { id } = req.params;
+  try {
+    const [news] = await db.execute("SELECT * FROM news WHERE id = ?", [id]);
+    if (news.length === 0) {
+      res.status(404).json({
+        status: "error",
+        message: "News not found",
+      });
+    }
+
+    res.json({
+      status: "success",
+      data: news[0],
+    });
+  } catch (error) {
+    console.error("error getting news", error);
+    res.status(500).json({
+      status: "error",
+      message: "Failed to get new by id",
+    });
+  }
+};
+
+const updateNews = async (req, res) => {
+  const { title, description, image_url, source } = req.body;
+  const { id } = req.params;
+  const db = req.app.locals.db;
+
+  try {
+    const [existing] = await db.execute("SELECT id FROM news WHERE id = ?", [
+      id,
+    ]);
+    if (existing.length === 0) {
+      return res.status(404).json({
+        status: "error",
+        message: "News not found. Failed to find the news.",
+      });
+    }
+    await db.execute(
+      "UPDATE news SET title = ?, description= ?, image_url = ?, source = ? WHERE id = ?",
+      [title, description, image_url, source, id]
+    );
+    res.json({
+      status: "success",
+      data: { id: parseInt(id), title, description, image_url, source },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to update news",
+    });
+  }
+};
+
+const deleteNews = async (req, res) => {
+  const { id } = req.params;
+  const db = req.app.locals.db;
+
+  try {
+    const [existing] = await db.execute("SELECT id FROM news WHERE id = ?", [
+      id,
+    ]);
+    if (existing.length === 0) {
+      return res.status(404).json({
+        status: "error",
+        message: "News not found. Failed to delete.",
+      });
+    }
+    await db.execute("DELETE FROM news WHERE id =?", [id]);
+    res.json({
+      status: "success",
+      message: "Successfully deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to delete news.",
+    });
+  }
+};
+module.exports = { createNews, getNews, getNewsById, updateNews, deleteNews };
--- routes/news.route.js
+++ routes/news.route.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { createNews } = require("../controllers/news.controller");
+const {
+  createNews,
+  getNews,
+  getNewsById,
+  updateNews,
+  deleteNews,
+} = require("../controllers/news.controller");
 
 const router = express.Router();
 router.post("/", createNews);
+router.get("/", getNews);
+router.get("/:id", getNewsById);
+router.put("/:id", updateNews);
+router.delete("/:id", deleteNews);
 
 module.exports = router;
